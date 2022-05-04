59f8d36b79023453ed3ff8f6369a7a559f121178
youzhao.zhou <youzhao.zhou@verystar.cn>
2022-05-04 23:12:21 +0800

    feat: Init Project

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function index() {
+    return "my-doc";
+}
+exports.default = index;
+//# sourceMappingURL=index.js.map
---
+++ dist/my-doc.es.js
@@ -0,0 +1,6 @@
+function index() {
+    return "my-doc";
+}
+
+export { index as default };
+//# sourceMappingURL=my-doc.es.js.map
